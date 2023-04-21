c6efad878e8d00b9e063e5963106825187a78f20
me <25042646+properchopsticks@users.noreply.github.com>
2023-04-21 20:00:40 +0200

    initial commit

---
+++ build.rs
@@ -0,0 +1,31 @@
+use bindgen::{self, CargoCallbacks, Bindings, builder};
+
+
+fn main() -> () {
+
+    // check if bindings.rs has changed
+    println!("cargo:rerun-if-changed=./src/bindings.rs");
+
+    // create bindings object that includes all the allowed functions from the header files
+    let bindings: Bindings = builder()
+        .header("stdlib.h")
+        .header("/usr/include/python3.10/Python.h")
+        .allowlist_type("PyStatus")
+        .allowlist_type("PyPreConfig")
+        .allowlist_type("PyConfig")
+        .allowlist_function("PyConfig_SetBytesString")
+        .allowlist_function("PyPreConfig_InitIsolatedConfig")
+        .allowlist_function("Py_PreInitialize")
+        .allowlist_function("PyConfig_InitIsolatedConfig")
+        .allowlist_function("Py_InitializeFromConfig")
+        .allowlist_function("Py_RunMain")
+        .parse_callbacks(Box::new(CargoCallbacks))
+        .generate()
+        .expect("unable to generate bindings");
+
+    // write the bindings to ./src/bindings.rs
+    bindings
+        .write_to_file("./src/bindings.rs")
+        .expect("could not write bindings");
+
+    return}
---
+++ src/main.rs
@@ -0,0 +1,64 @@
+// declaration for bindgen to include the generated bindings
+#![allow(non_upper_case_globals)]
+#![allow(non_camel_case_types)]
+#![allow(non_snake_case)]
+#![allow(unused_variables)]
+
+
+use std::ffi::{CString, c_int};
+use std::mem::{MaybeUninit};
+
+
+include!("./bindings.rs");
+
+
+// python rust architecture
+// python interpreter embedded into rust complete with modules and script
+// no interpreter or scrips in the folder
+// no writing to file
+// dlls are allowed
+
+
+extern "C" {
+    pub fn abs(number: c_int) -> c_int;}
+
+
+// main function
+fn main() -> () {
+
+    let mut python_status: PyStatus = unsafe {MaybeUninit::<PyStatus>::zeroed().assume_init()};
+    let mut python_preconfig: PyPreConfig = unsafe {MaybeUninit::<PyPreConfig>::zeroed().assume_init()};
+    let mut python_config: PyConfig = unsafe {MaybeUninit::<PyConfig>::zeroed().assume_init()};
+
+    let python_code: CString = CString::new(include_str!("../c_stuff/python1.py")).unwrap();
+    let python_name: CString = CString::new("my python program").unwrap();
+    let python_home: CString = CString::new("/usr").unwrap();
+
+    // initialize preconfig struct
+    unsafe {PyPreConfig_InitIsolatedConfig(&mut python_preconfig)};
+    // python_preconfig.configure_locale = 1;
+    // python_preconfig.coerce_c_locale = 2;
+    python_preconfig.isolated = 1;
+    python_preconfig.utf8_mode = 1;
+    python_status = unsafe {Py_PreInitialize(&mut python_preconfig)};
+    println!("preinitialized with exit code: {}", python_status.exitcode);
+
+    // initialize config struct
+    unsafe {PyConfig_InitIsolatedConfig(&mut python_config)};
+    // python_config.isolated = 1;
+    // python_config.interactive = 1;
+    python_config.install_signal_handlers = 1;
+    python_config.verbose = 0;
+    unsafe {
+        PyConfig_SetBytesString(&mut python_config, &mut python_config.program_name, python_name.as_ptr());
+        PyConfig_SetBytesString(&mut python_config, &mut python_config.home, python_home.as_ptr());
+        PyConfig_SetBytesString(&mut python_config, &mut python_config.run_command, python_code.as_ptr());
+        Py_InitializeFromConfig(&mut python_config);}
+    println!("initialized with exit code: {}", python_status.exitcode);
+
+    // run python
+    unsafe {Py_RunMain()};
+
+    println!("finished running Py_RunMain");
+
+    return}
