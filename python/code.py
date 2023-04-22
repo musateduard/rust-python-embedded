c6f75412919b1ec183adda4ecb1670bfba4f981f
me <25042646+properchopsticks@users.noreply.github.com>
2023-04-23 00:30:06 +0200

    added test functions

--- python/code.py
+++ python/code.py
@@ -1,4 +1,20 @@
 import time
+import websockets
+import asyncio
+
+
+async def test_function():
+    print(f"client connected")
+    return
+
+
+async def main():
+
+    async with websockets.serve(test_function, host=None, port=2233, ping_timeout=None):
+        print(f"current time is: {time.asctime()}")
+
+    return
+
 
 if __name__ == "__main__":
-    print(f"current time is: {time.asctime()}")
+    asyncio.run(main(), debug=False)
